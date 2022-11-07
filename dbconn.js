9e3bafeca085e964a5750f78f30bd078ff124140
Saqib Ali <Admin@USERTL11.tpsonline.com>
2022-11-07 13:49:46 +0500

    first commit1

--- dbconn.js
+++ dbconn.js
@@ -1,55 +1,37 @@
 const {Pool, Client} = require('pg');
 
-// const pool = new Pool({
-//     user: "niuksbuokhokem",
-//     host: "ec2-44-205-177-160.compute-1.amazonaws.com",
-//     database: "de8ttt3jrv7o65",
-//     password: "65e797b06cc4f90dc2831cdd95145e720de8b6e639ed9b0f7c5ed2bf70122268",
-//     port: "5432",
-// });
-
 const credentials = {
     user: "niuksbuokhokem",
     host: "ec2-44-205-177-160.compute-1.amazonaws.com",
     database: "de8ttt3jrv7o65",
     password: "65e797b06cc4f90dc2831cdd95145e720de8b6e639ed9b0f7c5ed2bf70122268",
     port: "5432",
+    ssl: {
+        rejectUnauthorized: false
+    }
   };
 
+const pool = new Pool(credentials);
 
-// Connect with a connection pool.
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
+  const client = new Client(credentials);
+  client.connect()
+  client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    client.end()
+  })
 
-async function poolDemo() {
-    const pool = new Pool(credentials);
-    const now = await pool.query("SELECT NOW()");
-    await pool.end();
-  
-    return now;
+  async function registerPerson(person) {
+    const text = `
+      INSERT INTO users (user_id, user_name)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id
+    `;
+    const values = [person.fullname, person.gender, person.phone, person.age];
+    return pool.query(text, values);
   }
-  
-  // Connect with a client.
-  
-  async function clientDemo() {
-    const client = new Client(credentials);
-    await client.connect();
-    const now = await client.query("SELECT NOW()");
-    await client.end();
-  
-    return now;
-  }
-  
-  // Use a self-calling function so we can use async / await.
-  
-  (async () => {
-    const poolResult = await poolDemo();
-    console.log("Time with pool: " + poolResult.rows[0]["now"]);
-  
-    const clientResult = await clientDemo();
-    console.log("Time with client: " + clientResult.rows[0]["now"]);
-  })();
-
-
-var query = client.query("SELECT * FROM users");
-
 
 module.exports = pool;
